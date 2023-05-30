c6137d76ed8e0c7d1b965445bbe1866f6574e86f
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-05-30 11:43:55 -0500

    Create music album and genre tables in Schema.sql

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE catalog;
+
+CREATE TABLE
+    music_album (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        genre_id INTEGER NOT NULL,
+        author_id INTEGER NOT NULL,
+        source_id INTEGER NOT NULL,
+        label_id INTEGER NOT NULL,
+        publish_date DATE NOT NULL,
+        archived BOOLEAN NOT NULL,
+        FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE,
+    );
+
+CREATE TABLE
+    genre (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+    );
