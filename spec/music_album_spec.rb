13eb25545ce1be48e994d6a2c1168adf1a21a548
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-05-30 16:44:49 -0500

    Test initialize of `MusicAlbum` class

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,6 +1,20 @@
 require_relative '../music_album'
 
 describe MusicAlbum do
+  describe '#initialize' do
+    before(:each) do
+      @album = MusicAlbum.new(true, 11, false, 'Title')
+    end
+
+    it 'creates a new instance of MusicAlbum' do
+      expect(@album).to be_an_instance_of(MusicAlbum)
+    end
+
+    it 'should have attributes' do
+      expect(@album).to have_attributes(on_spotify: true, publish_date: 11, archived: false, title: 'Title')
+    end
+  end
+
   describe '#can_be_archived?' do
     it 'returns true if the album is on spotify and can be archived' do
       album = MusicAlbum.new(true, 11, false, 'Title')
