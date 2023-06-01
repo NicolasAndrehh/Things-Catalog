9c6e4f8e072e985efeca6d6557999bde696980d1
Rashed Arman <rashedarman21@gmail.com>
2023-06-01 22:35:55 +0600

    feat: create author class and test

---
+++ author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :first_name, :last_name, :items
+
+  def initialize(first_name, last_name)
+    @id = rand(1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    item.author = self
+  end
+end
